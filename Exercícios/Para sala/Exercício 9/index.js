1a9849f4c6deaa1a147ebf97d0a976b617c07c57
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-07-29 16:16:37 -0300

    resolução exercício 9

--- Exercícios/Para sala/Exercício 9/index.js
+++ Exercícios/Para sala/Exercício 9/index.js
@@ -22,6 +22,25 @@ class Funcionario {
   }
 }
 
-const funcionario1 = new Funcionario('Marta', 12345678, 1000);
-funcionario1.receberAumento();
-funcionario1.exibirDados();
+// const funcionario1 = new Funcionario('Marta', 12345678, 1000);
+// funcionario1.receberAumento();
+// funcionario1.exibirDados();
+
+class Gerente extends Funcionario {
+  nivelGerencia;
+  aumento = 0.15;
+
+  constructor(nome, cpf, salario, nivelGerencia) {
+    super(nome, cpf, salario);
+    this.nivelGerencia = nivelGerencia;
+  }
+
+  exibirDados() {
+    super.exibirDados();
+    console.log(`Nível de gerência: ${this.nivelGerencia}`);
+  }
+}
+
+const gerente1 = new Gerente('Debora', 123456789, 1000, 'Junior');
+gerente1.receberAumento();
+gerente1.exibirDados();
