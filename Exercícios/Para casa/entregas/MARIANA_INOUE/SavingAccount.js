b6837b0e310c2c0acb1f6c8fc842b94842a72162
marinoue <marinoue@gmail.com>
2023-07-30 19:58:07 -0300

    refactor: remove-function-tranfer-to

--- Exercícios/Para casa/entregas/MARIANA_INOUE/SavingAccount.js
+++ Exercícios/Para casa/entregas/MARIANA_INOUE/SavingAccount.js
@@ -29,48 +29,6 @@ class SavingAccount extends BankAccount {
         this.#withdrawalTax = newWithdrawalTax
     }
 
-
-
-    // transferTo(anotherAccount, amount) {
-    //     if (!(anotherAccount instanceof BankAccount)) {
-    //         console.log('Informe uma conta válida!');
-    //         return;
-    //     }
-
-    //     let amountToBeDebited = amount;
-    //     if (this.bank.bankCode !== anotherAccount.bank.bankCode) {
-    //         amountToBeDebited = amount + amount * this.bank.transferTax;
-    //         console.log(
-    //             `Essa transferência terá uma taxa de ${this.bank.transferTax * 100
-    //             }%, pois se trata de uma transferência entre bancos diferentes.`
-    //         );
-    //     }
-
-    //     if (this.#balance >= amountToBeDebited) {
-    //         this.#balance -= amountToBeDebited;
-    //         anotherAccount.balance += amount;
-
-    //         console.log(`O saldo atual da conta de origem é de R$ ${this.#balance}`);
-    //         console.log(
-    //             `O saldo atual da conta de destino é de R$ ${anotherAccount.balance}`
-    //         );
-    //     } else {
-    //         console.log(
-    //             `Saldo insuficiente para realizar a transferência. Seu saldo atual é de ${this.#balance
-    //             }. Para realizar essa transferência você precisa ter ${amountToBeDebited} em conta.`
-    //         );
-    //     }
-    //}
-
-    // creditAmountRate(amount){
-    //      let rateValue = this.incomeRate * 100
-    //      amount = amount * rateValue
-    //      this.creditAmount(amount)
-
-    // }
-
-
-
     generateIncome(currentDay) {
         if (this.incomeDay === currentDay) {
             this.balanceAdd(this.incomeRate)
