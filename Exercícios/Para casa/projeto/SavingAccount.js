6eeb9fbca3b944e0ccabc52af5e6cd9596436d14
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-01 20:12:22 -0300

    feat: exercício feito em aula

---
+++ Exercícios/Para casa/projeto/SavingAccount.js
@@ -0,0 +1,41 @@
+const { BankAccount } = require("./BankAccount");
+
+class SavingAccount extends BankAccount {
+  #qtdWithdrawal = 0;
+  #MAX_OF_WITHDRAWAL = 2; // Letras maiusculas para constantes - esse valor não vai mudar.
+  #withdrawalTax = 0.03;
+
+  cashWithdrawal(amount) {
+    console.log(
+      `Você já realizou ${this.#qtdWithdrawal} retiradas. Você tem direito a ${
+        this.#MAX_OF_WITHDRAWAL
+      } retiradas gratuitas mensais.`
+    );
+
+    if (this.#qtdWithdrawal < this.#MAX_OF_WITHDRAWAL) {
+      if (this.balance >= amount) {
+        this.balance -= amount;
+        this.#qtdWithdrawal++;
+        console.log(
+          `Retirada realizada com sucesso. Seu saldo restante é ${this.balance},00.`
+        );
+      } else {
+        console.log(`Você não tem saldo suficiente para essa operação.`);
+      }
+    } else {
+      const amountWithTax = amount + amount * this.#withdrawalTax;
+
+      if (this.balance >= amountWithTax) {
+        this.balance -= amountWithTax;
+        this.#qtdWithdrawal++;
+        console.log(
+          `Retirada realizada com sucesso. Seu saldo restante é ${this.balance},00.`
+        );
+      } else {
+        console.log(`Você não tem saldo suficiente para essa operação.`);
+      }
+    }
+
+    console.log(`Você já realizou ${this.#qtdWithdrawal} retiradas.`);
+  }
+}
