391ab346a07f007058baf70a9861d0b721d223eb
carolmarquezini <c.marquezini15@gmail.com>
2023-08-01 21:42:22 -0300

    Adicionando exercício 4 resolvido em aula..

--- Exercícios/Para sala/Exercício 4/index.js
+++ Exercícios/Para sala/Exercício 4/index.js
@@ -1,1 +1,24 @@
+class Ingresso {
+    preco = 10.00;
 
+    mostrarPreco() {
+        console.log(`O preco do ingresso: R$ ${this.preco}`)
+    }
+}
+
+class IngressoNormal extends Ingresso {
+
+}
+
+class IngressoVIP extends Ingresso {
+    preco = 50;
+
+}
+
+const ingresso1 = new Ingresso();
+ingresso1.mostrarPreco();
+
+const ingressoNormal1 = new IngressoNormal();
+
+const ingressoVIP1 = new IngressoVIP();
+ingressoVIP1.mostrarPreco();
