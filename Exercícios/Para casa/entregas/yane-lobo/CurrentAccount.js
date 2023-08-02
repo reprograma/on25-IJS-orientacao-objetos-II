180170c2f8610e2129318532d67549e483974492
Yane <yanelobo@gmail.com>
2023-08-02 15:41:37 -0300

    Resolução do exercicio semanal

--- Exercícios/Para casa/entregas/yane-lobo/CurrentAccount.js
+++ Exercícios/Para casa/entregas/yane-lobo/CurrentAccount.js
@@ -1,16 +1,6 @@
 const { BankAccount } = require('./BankAccount');
 
 class CurrentAccount extends BankAccount {
-
-/*     cashWithdrawal(amount){
-        if(!(amount <= this.balance)){
-            console.log('Você não tem saldo suficiente para realizar essa operação!')
-        } else{
-            this.balance -= amount
-            console.log(`Retirada realizada com sucesso. Seu saldo restante é R$ {$this.balance}`)
-        }
-    } */
-
     
     transferTo(anotherAccount, amount){
         if (!(anotherAccount instanceof BankAccount)) {
