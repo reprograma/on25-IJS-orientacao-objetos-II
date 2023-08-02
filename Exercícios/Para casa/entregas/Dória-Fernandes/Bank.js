ef1d37180b07bcc31e8bb526ce769fb9642362f1
DoriansFer <doohfernandes@yahoo.com>
2023-08-01 21:48:51 -0300

    mesclagem

---
+++ Exercícios/Para casa/entregas/Dória-Fernandes/Bank.js
@@ -0,0 +1,23 @@
+class Bank {
+	bankCode;
+	bankName;
+	#trasferTax;
+
+	static createdBanks = [];
+
+	constructor(bankCode, bankName, transferTax) {
+		this.bankCode = bankCode;
+		this.bankName = bankName;
+		this.#trasferTax = transferTax;
+		this.constructor.createdBanks.push({
+			bankCode: this.bankCode,
+			qtdClients: 0,
+		});
+	}
+
+	get transferTax() {
+		return this.#trasferTax;
+	}
+}
+
+module.exports = { Bank };
---
+++ Exercícios/Para casa/entregas/Dória-Fernandes/SavingAccount.js
@@ -0,0 +1,112 @@
+const { Bank } = require('./Bank');
+const { Client } = require('./Client');
+const { BankAccount } = require('./BankAccount');
+
+class SavingAccount extends BankAccount {
+    #balance = 0;
+    incomeRate;
+    incomeDay;
+    #qtdWithdrawal = 0;
+    #withdrawalTax = 7;
+    MAX_OF_WITHDRAWAL = 2;
+
+    constructor(client, bank, accountNumber, agencyNumber, incomeDay, incomeRate) {
+        super(client, bank, accountNumber, agencyNumber);
+        this.incomeDay = incomeDay;
+        this.incomeRate = incomeRate;
+    }
+
+    get qtdWithdrawal() {
+        return this.#qtdWithdrawal;
+    }
+
+    get withdrawalTax() {
+        return this.#withdrawalTax;
+    }
+
+    set withdrawalTax(newWithdrawaTax) {
+        this.#withdrawalTax += newWithdrawaTax;
+    }
+
+    cashWithdrawal(amount) {
+        if (this.#qtdWithdrawal >= this.MAX_OF_WITHDRAWAL && amount <= this.#balance) {
+            this.#balance -= amount;
+            this.#qtdWithdrawal += 1;
+            console.log(`As primeiras 2 retiradas são gratuitas. `)
+            console.log(`Foi cobrado uma taxa de ${this.#withdrawalTax} `)
+            console.log(`Sacou o valor de ${amount - this.#withdrawalTax} `)
+            console.log(`O saldo atual é de R$${this.#balance}. `)
+            console.log(`Você realizou ${this.#qtdWithdrawal} saque(s). `)
+            console.log(`Você não possui mais nenhuma retirada gratuita. `)
+            return;
+        }
+        if (this.#qtdWithdrawal == 1 && amount <= this.#balance) {
+            this.#balance -= amount;
+            this.#qtdWithdrawal += 1;
+            console.log(`As primeiras 2 retiradas são gratuitas. `)
+            console.log(`Sacou o valor de ${amount} `)
+            console.log(`O saldo atual é de R$${this.#balance}. `)
+            console.log(`Você realizou ${this.#qtdWithdrawal} saque(s). `)
+            console.log(`Você não possui mais nenhuma retirada gratuita. `)
+            return;
+        } if (this.#qtdWithdrawal == 0 && amount <= this.#balance) {
+            this.#balance -= amount;
+            this.#qtdWithdrawal += 1;
+            console.log(`As primeiras 2 retiradas são gratuitas. `)
+            console.log(`Sacou o valor de ${amount} `)
+            console.log(`O saldo atual é de R$${this.#balance}. `)
+            console.log(`Você realizou ${this.#qtdWithdrawal} saque(s). `)
+            console.log(`Você ainda possui 1 retirada gratuita. `)
+            return;
+        } if (amount > this.#balance) {
+            return console.log(`Você não possui saldo o suficiente`)
+        } else {
+            console.log("Ops, ocorreu algum erro")
+        }
+    }
+
+    generateIncome(currentDay) {
+        if (currentDay == this.incomeDay) {
+            this.#balance += this.#balance * this.incomeRate;
+            console.log(`Seu novo saldo é ${this.#balance}`)
+            return;
+        }
+    }
+
+}
+
+module.exports = { SavingAccount };
+const { BankAccount } = require('./BankAccount');
+
+class SavingAccount extends BankAccount {
+  #qtdWithdrawal = 0;
+  #MAX_OF_WITHDRAWAL = 2;
+  #withdrawalTax = 0.03;
+
+  cashWithdrawal(amount) {
+    console.log(`Você já realizou ${this.#qtdWithdrawal} retiradas. Você tem direito a ${this.#MAX_OF_WITHDRAWAL} retiradas gratuitas.`)
+
+    if(this.#qtdWithdrawal < this.#MAX_OF_WITHDRAWAL) {
+      if(this.balance >= amount) {
+        this.balance -= amount;
+        this.#qtdWithdrawal++;
+        console.log(`Retirada realizada com sucesso. Seu saldo restante é R$ ${this.balance},00`)
+      } else {
+        console.log(`Você não tem saldo suficiente para essa operação;`);
+      }
+    } else {
+      const amountWithTax = amount + (amount * this.#withdrawalTax);
+      if(this.balance >= amountWithTax) {
+        this.balance -= amountWithTax;
+        this.#qtdWithdrawal++;
+        console.log(`Retirada realizada com sucesso. Seu saldo restante é R$ ${this.balance},00`)
+      } else {
+        console.log(`Você não tem saldo suficiente para essa operação;`);
+      }
+    }
+
+    console.log(`Você já realizou ${this.#qtdWithdrawal} retiradas.`);
+  }
+}
+
+module.exports = { SavingAccount };
