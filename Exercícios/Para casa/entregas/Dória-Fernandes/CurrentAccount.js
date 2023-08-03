76332cfd3c7c06b0b1aad72921ae43b331042a9c
DoriansFer <doohfernandes@yahoo.com>
2023-08-03 09:58:40 -0300

    Current Account e Bank Account corrigido

--- Exercícios/Para casa/entregas/Dória-Fernandes/CurrentAccount.js
+++ Exercícios/Para casa/entregas/Dória-Fernandes/CurrentAccount.js
@@ -8,20 +8,18 @@ class CurrentAccount extends BankAccount {
     }
 
     transferTo(anotherAccount, amount) {
-        super.transferTo();
-        if (!(anotherAccount instanceof BankAccount)) {
+        if (!(anotherAccount instanceof CurrentAccount)) {
             console.log('Informe uma conta válida!');
             return;
         } else {
             if (amount > this.balance) {
-                return console.log(`Seu saldo atual é insuficiente para realizar essa transferência`);
+                return console.log(`Seu saldo atual é insuficiente para realizar essa transferência. Seu saldo atual é de R$${this.balance},00`);
             } else {
                 const anotherClientName = anotherAccount.client ? anotherAccount.client.name : 'cliente desconhecido';
-                const anotherBank = anotherAccount.bank;
-                if (amount <= this.balance && anotherBank == this.bank) {
+                if (amount <= this.balance) {
                     anotherAccount.balance += amount;
                     this.balance -= amount;
-                    return console.log(`Transferência realizada com sucesso para ${anotherClientName}. Seu saldo atual é de R$ ${this.balance}`);
+                    return console.log(`Transferência realizada com sucesso para ${anotherClientName}. Seu saldo atual é de R$ ${this.balance},00`);
                 } else {
                     return console.log('Ops, ocorreu um erro. Tente novamente mais tarde!');
                 }
