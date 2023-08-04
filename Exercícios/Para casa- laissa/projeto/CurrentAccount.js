47d75309d84367ebe9cd13b5dfb4eec920f70816
Laissa Cunha <laissa.cunha@ed3.com.br>
2023-08-04 19:58:26 -0300

    feat: Sobrescreve o método transferTo(anotherAccount, amount)

---
+++ Exercícios/Para casa- laissa/projeto/CurrentAccount.js
@@ -0,0 +1,32 @@
+const {BankAccount} = require("./BankAccount");
+
+class CurrentAccount extends BankAccount {
+  #balance;
+
+  transferTo(anotherAccount, amount) {
+    if (!(anotherAccount instanceof BankAccount)) {
+      console.log("Informe uma conta válida!");
+      return;
+    }
+
+    let amountToBeDebited = amount;
+   
+    if (this.#balance >= amountToBeDebited) {
+      this.#balance -= amountToBeDebited;
+      anotherAccount.balance += amount;
+
+      console.log(`O saldo atual da conta de origem é de R$ ${this.#balance}`);
+      console.log(
+        `O saldo atual da conta de destino é de R$ ${anotherAccount.balance}`
+      );
+    } else {
+      console.log(
+        `Saldo insuficiente para realizar a transferência. Seu saldo atual é de ${
+          this.#balance
+        }. Para realizar essa transferência você precisa ter ${amountToBeDebited} em conta.`
+      );
+    }
+  }
+}
+
+module.exports = {CurrentAccount};
