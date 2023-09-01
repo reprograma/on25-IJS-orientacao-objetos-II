1779a29679edb6aa75028926bd6dc31afd6d13ef
Deborah Gomes Souza <deborah270691@gmail.com>
2023-09-01 17:00:15 -0300

    bank

--- Exercícios/Para casa/entregas/deborah-gomes/Bank.js
+++ Exercícios/Para casa/entregas/deborah-gomes/Bank.js
@@ -2,11 +2,7 @@ class Bank {
 	bankCode;
 	bankName;
 	#trasferTax;
-<<<<<<< HEAD:Exercícios/Para casa/entregas/deborah-gomes/Bank.js
 	#managers = [];
-=======
-
->>>>>>> cfb03f494e35750f49cb5ed6f1e03b0fb3c2b23b:Exercícios/Para casa/projeto/Bank.js
 	static createdBanks = [];
 
 	constructor(bankCode, bankName, transferTax) {
@@ -22,15 +18,12 @@ class Bank {
 	get transferTax() {
 		return this.#trasferTax;
 	}
-<<<<<<< HEAD:Exercícios/Para casa/entregas/deborah-gomes/Bank.js
 
 	contractManager(managers) {
 		this.#managers.push(managers);
 		console.log(`Gerente ${this.#managers.name} contratado para o banco ${this.bankName}`);
 	  }
 	
-=======
->>>>>>> cfb03f494e35750f49cb5ed6f1e03b0fb3c2b23b:Exercícios/Para casa/projeto/Bank.js
 }
 
 module.exports = { Bank };
